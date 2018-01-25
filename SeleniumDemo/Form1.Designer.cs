eb3cb26ca5af06d0e93b270fff3ec49a171456f4
DikshaGupta1015 <34881041+DikshaGupta1015@users.noreply.github.com>
2018-01-25 18:18:01 +0530

    Add project files.

---
+++ SeleniumDemo/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace SeleniumDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStartChrome = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStartChrome
+            // 
+            this.btnStartChrome.Location = new System.Drawing.Point(12, 12);
+            this.btnStartChrome.Name = "btnStartChrome";
+            this.btnStartChrome.Size = new System.Drawing.Size(75, 42);
+            this.btnStartChrome.TabIndex = 0;
+            this.btnStartChrome.Text = "Start Chrome";
+            this.btnStartChrome.UseVisualStyleBackColor = true;
+            this.btnStartChrome.Click += new System.EventHandler(this.btnStartChrome_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Navigate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(174, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(12, 90);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.TabIndex = 3;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnForward
+            // 
+            this.btnForward.Location = new System.Drawing.Point(93, 90);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(75, 23);
+            this.btnForward.TabIndex = 4;
+            this.btnForward.Text = "Forward";
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(174, 90);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 5;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(31, 176);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(255, 20);
+            this.txtUrl.TabIndex = 6;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(210, 216);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 7;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(84, 216);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 8;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 261);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnStartChrome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartChrome;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ SeleniumDemo/Form1.cs
@@ -0,0 +1,70 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeleniumDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        IWebDriver driver;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //driver = new ChromeDriver();
+            driver.Url = "http://www.google.com";
+            System.Threading.Thread.Sleep(5000);
+            driver.Url = "http://www.w3schools.com";
+        }
+
+        private void btnStartChrome_Click(object sender, EventArgs e)
+        {
+            driver = new ChromeDriver();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            driver.Close();
+            driver.Quit();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            driver.Navigate().Back();
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            driver.Navigate().Forward();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            driver.Navigate().Refresh();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            driver.Url = "file:///D:/Users/Learning/Downloads/default.html";
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            IWebElement element = driver.FindElement(By.Name("q"));
+            element.Clear();
+            element.SendKeys(txtUrl.Text);
+            element.Submit();
+        }
+    }
+}
